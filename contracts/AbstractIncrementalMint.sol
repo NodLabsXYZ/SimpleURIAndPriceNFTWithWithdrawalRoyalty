3ffcb4fe8d768a202ef20abfd446d06cae16e405
Jared Cosulich <jared.cosulich@gmail.com>
2022-03-14 13:47:36 -0400

    append json to the end of the tokenUrl

--- contracts/AbstractIncrementalMint.sol
+++ contracts/AbstractIncrementalMint.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 abstract contract AbstractIncrementalMint is ERC721 {
@@ -20,14 +22,26 @@ abstract contract AbstractIncrementalMint is ERC721 {
         _baseTokenURI = baseTokenURI;
     }
 
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(_exists(tokenId), "URI query for nonexistent token");
+
+        string memory base = _baseURI();
+
+        return
+            string(abi.encodePacked(base, Strings.toString(tokenId), ".json"));
+    }
+
     function _baseURI() internal view virtual override returns (string memory) {
         return _baseTokenURI;
     }
 
-    function _mintNextToken()
-        internal
-        returns (uint256)
-    {
+    function _mintNextToken() internal returns (uint256) {
         _tokenIdTracker.increment();
         uint256 tokenId = _tokenIdTracker.current();
         _safeMint(_msgSender(), tokenId);
